71626d383ad9bae0803f5602c30d848867266748
Vasiliy Bout <bout.vasiliy@gmail.com>
2010-11-02 02:58:49 +0300

    build_packages changes
    Better processing paths with spaces

--- build_packages.bat
+++ build_packages.bat
@@ -51,43 +51,43 @@ exit /B 0
 
 
 :create_tmpdir
-	mkdir %tmpdir%
-	copy Makefile.win32 %tmpdir%
-	copy build_packages.bat %tmpdir%
-	xcopy /Q /I /E zlibrary %tmpdir%\zlibrary
-	xcopy /Q /I /E fbreader %tmpdir%\fbreader
-	xcopy /Q /I /E makefiles %tmpdir%\makefiles
+	mkdir "%tmpdir%"
+	copy Makefile.win32 "%tmpdir%"
+	copy build_packages.bat "%tmpdir%"
+	xcopy /Q /I /E zlibrary "%tmpdir%\zlibrary"
+	xcopy /Q /I /E fbreader "%tmpdir%\fbreader"
+	xcopy /Q /I /E makefiles "%tmpdir%\makefiles"
 
-	xcopy /Q /I /E deps %tmpdir%\deps
-	xcopy /Q /I /E makefiles-win32 %tmpdir%\makefiles-win32
+	xcopy /Q /I /E deps "%tmpdir%\deps"
+	xcopy /Q /I /E makefiles-win32 "%tmpdir%\makefiles-win32"
 
-	copy README.build %tmpdir%
-	copy CHANGES* %tmpdir%
-	xcopy /Q /I /E distributions %tmpdir%\distributions
-	copy ChangeLog %tmpdir%
+	copy README.build "%tmpdir%"
+	copy CHANGES* "%tmpdir%"
+	xcopy /Q /I /E distributions "%tmpdir%\distributions"
+	copy ChangeLog "%tmpdir%"
 
-	for /R %tmpdir% %%D IN (.) DO (
+	for /R "%tmpdir%" %%D IN (.) DO (
 		IF "%%~nxD"==".svn" (
-			rmdir /Q /S %%D
+			rmdir /Q /S "%%D"
 		)
 	)
 
-	pushd %tmpdir%
+	pushd "%tmpdir%"
 rem	nmake /nologo /f Makefile.win32 clean
 	popd
 exit /B 0
 
 
 :remove_tmpdir
-	rmdir /S /Q %tmpdir%
+	rmdir /S /Q "%tmpdir%"
 exit /B 0
 
 
 :build_package
-	SET make_package=nmake /nologo /f "makefiles-win32\packaging.mk" DIST_DIR=%distdir% VERSION=%version% ARCHITECTURE=%1 %2
+	SET make_package=nmake /nologo /f "makefiles-win32\packaging.mk" DIST_DIR="%distdir%" VERSION="%version%" ARCHITECTURE=%1 %2
 
 	IF "%2" == "nsi" (
-		pushd %tmpdir%
+		pushd "%tmpdir%"
 		%make_package%
 		IF ERRORLEVEL 1 (
 			echo Building failed... abort.
@@ -96,8 +96,8 @@ exit /B 0
 			exit /B 0
 		)
 		popd
-		mkdir %pkgdir%\%1
-		move /Y %tmpdir%\*.exe %pkgdir%\%1
+		mkdir "%pkgdir%\%1"
+		move /Y "%tmpdir%\*.exe" "%pkgdir%\%1"
 	) else (
 		echo No rule is defined for package type "$2"
 	)
